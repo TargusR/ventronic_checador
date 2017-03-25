6db251a38780aa5b7aeba4dc48738068a984c835
Josué Martín <josue@matrushka.com.mx>
2017-03-25 02:34:21 -0600

    Adding Model Product script content

---
+++ app/javascripts/models/model_product.js
@@ -0,0 +1,31 @@
+define([
+  'backbone'
+  ],
+
+  function(Backbone) {
+    var ProductModel = Backbone.Model.extend({
+
+      fetchProduct: function(barcode, callbackOn, callbackOff) {
+        var model = this;
+        model.url = C.api + '?pr=' + barcode;
+        model.data_type = 'Producto';
+        var ProductoSuccess = function(model, response, options) {
+          callbackOn();
+        }
+        var ProductoError = function() {
+          console.log("ERROR: Can't reach data source");
+          callbackOff();
+        }
+        model.fetch({
+          success: ProductoSuccess,
+          error: ProductoError,
+          reset: false,
+          remove: false
+        })
+      }
+
+    });
+
+    return ProductModel;
+  }
+);
