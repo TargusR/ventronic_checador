77f63b783cb0b8b62119b18beb4b68b37058f707
Josué Martín <josue@matrushka.com.mx>
2017-04-17 10:48:03 -0500

    Adding config.info file configuration methods

--- app/javascripts/main/config.js
+++ app/javascripts/main/config.js
@@ -4,17 +4,50 @@ define([
 
 function($) {
   var config = {
-    api: 'http://local.test.com/chuy/',
-    image_server: ''
+    // default Values
+    api: 'default',
+    image_server: 'default'
+  }
+
+  config.fileGet = function ( file ) {
+    var c = this;
+    var fs = require('fs');
+    var fileParser = this.fileParser;
+    var configValues = this.configValues;
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) throw err;
+      c.configValues(c.fileParser(data));
+    });
+  }
+
+  config.configValues = function ( data ) {
+    this.api = data['api'];
+    this.image_server = data['image_server'];
+  }
+
+  config.fileParser = function ( data ) {
+    var values = data.split('\n');
+    var table = [];
+    $(values).each(function (index, value) {
+      value = value.replace("\t",'');
+      value = value.split(' ');
+      table[value.shift()] = value.join();
+    });
+    return table;
+  }
+
+  config.initialize = function () {
+    var file = __dirname + '/config.info';
+    this.fileGet(file);
   }
 
   // Register globally
-  config.register = function(globalName) {
+  config.register = function( globalName ) {
     if(typeof(globalName) == 'undefined') {
       globalName = 'c';
     }
-
     window[globalName] = this;
+    this.initialize();
   }
 
   return config;
